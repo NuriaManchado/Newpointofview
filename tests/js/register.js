7a63778f2bb02cf9cf27c90b3d0e68bd31ddee75
ledezmatto <ledezmatto@tecnodesign.com.py>
2018-10-21 11:51:35 +0200

    Menu Moon - Mars

---
+++ tests/js/register.js
@@ -0,0 +1,30 @@
+/* global AFRAME */
+
+/**
+ * Component that listens to an event, fades out an entity, swaps the texture, and fades it
+ * back in.
+ */
+AFRAME.registerComponent('set-image', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+    src: {type: 'string'},
+    dur: {type: 'number', default: 300}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+
+    el.addEventListener(data.on, function () {
+      console.log("Ahora re-direcciona", data.src);
+      //window.location.href = "/luna.html";
+    });
+  },
+
+});
+
+
+
+
+
