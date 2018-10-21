f1d25de5b2d6c8df47fae04a71d4caa73b52e454
ledezmatto <ledezmatto@tecnodesign.com.py>
2018-10-21 13:44:25 +0200

    Back and Audio button working

--- tests/js/register.js
+++ tests/js/register.js
@@ -15,6 +15,7 @@ AFRAME.registerComponent('set-image', {
   init: function () {
     var data = this.data;
     var el = this.el;
+      //console.log("Funcion 1");
 
     el.addEventListener(data.on, function () {
       window.location.href = data.src;
@@ -22,8 +23,53 @@ AFRAME.registerComponent('set-image', {
     });
   },
 
-});
+})
 
+
+AFRAME.registerComponent('back', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+    src: {type: 'string'},
+    dur: {type: 'number', default: 300}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+      //console.log("Back Sound");
+
+    el.addEventListener(data.on, function () {
+      window.location.href = data.src;
+      console.log("Back");
+    });
+  },
+
+})
+
+AFRAME.registerComponent('audio', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+    src: {type: 'string'},
+    audio: {type: 'string'},
+    dur: {type: 'number', default: 300}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+      //console.log("Back Sound");
+
+    el.addEventListener(data.on, function () {
+      console.log(data.audio);
+      var audio = new Audio(data.audio);
+        audio.play();
+      console.log("Back");
+    });
+  },
+
+});
 
 
 
